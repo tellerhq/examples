b8e7b438260d9f6dc4faf982a837c3e1b3f0d7db
Dino Kovač <dino@teller.io>
2021-10-05 10:48:13 +0200

    Detect missing app id and warn user

--- static.sh
+++ static.sh
@@ -1,3 +1,15 @@
-# Starts a simple HTTP server to serve static assets
-# available in the `static` directory.
-python3 -m http.server --directory static 8000
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if egrep "APPLICATION_ID\s+=\s+'app_[a-zA-Z0-9]+'" static/index.js; then
+  # Starts a simple HTTP server to serve static assets
+  # available in the `static` directory.
+  python3 -m http.server --directory static 8000
+else
+  echo
+  echo "ERROR:"
+  echo "APPLICATION_ID not found in static/index.js"
+  echo
+  echo "Did you forget to set it? See README.md for instructions."
+  exit -1
+fi
